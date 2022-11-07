dad5eaec68f6e4fa822996c81e973c98a97b1425
Andrey-Vybornov <andrey.vybornoff@gmail.com>
2022-11-07 14:51:27 +0300

    test sign out

--- src/test/java/authorization/Authorization.java
+++ src/test/java/authorization/Authorization.java
@@ -16,35 +16,34 @@ import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 public class Authorization {
 
     AuthorizationUtils userEmail = new AuthorizationUtils();
+    AuthorizationUtils authorizationUtils = new AuthorizationUtils();
 
-    @BeforeAll
-    static void openWebSite() {
+
+    @BeforeEach
+    void openWebSite() {
         Configuration.browser = "Chrome";
         Configuration.browserSize = "1920x1080";
         open("https://portal.dev01.russpass.dev/");
+        authorizationUtils.successfulAuthorization();
 
     }
 
-    @AfterAll
-    static void closeWebSite() {
+    @AfterEach
+    void closeWebSite() {
         sleep(5000L);
         closeWindow();
     }
 
     @Test
     void successfulAuthorization() {
-        Configuration.holdBrowserOpen = true;
 
-
-//        open ("https://portal.dev01.russpass.dev/");
-//        $("a[href='/support']").click();
-//        $x("//*[@href='/support']").click();
-//        $x("//*[@class]//div[@class'']").setValue(AuthorizationUtils.EMAIL);
-        $x("//*[text()='Вход']").click();
-        $("#username").setValue(AuthorizationUtils.EMAIL);
-        $("#auth_ok").click();
-        $("#password").setValue(AuthorizationUtils.PASSWORD).pressEnter();
         $("[class=profile-new__header]").shouldHave(text(AuthorizationUtils.EMAIL));
+    }
+    @Test
+    void signOut() {
 
+       $("[class = 'button button--primary button--full-width cookie-banner-new__btn-ok']").click();
+       $x("//div[text()='Выйти из профиля']").click();
+       $x("//span[text()='Вход']").shouldHave(text("Вход"));
     }
 }
--- src/test/java/authorization/AuthorizationUtils.java
+++ src/test/java/authorization/AuthorizationUtils.java
@@ -1,10 +1,26 @@
 package authorization;
 
 
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class AuthorizationUtils {
     public static String EMAIL = "russ_test@ro.ru";
     public static String PASSWORD = "Test1234!";
 
+    void successfulAuthorization() {
+        Configuration.holdBrowserOpen = true;
 
+////        open ("https://portal.dev01.russpass.dev/");
+////        $("a[href='/support']").click();
+////        $x("//*[@href='/support']").click();
+////        $x("//*[@class]//div[@class'']").setValue(AuthorizationUtils.EMAIL);
+        $x("//*[text()='Вход']").click();
+        $("#username").setValue(AuthorizationUtils.EMAIL);
+        $("#auth_ok").click();
+        $("#password").setValue(AuthorizationUtils.PASSWORD).pressEnter();
+    }
 }
